36bad64b5b462ff5299d0a6d87739166fad72b45
MarcinK420 <marcin.kruk@uth.pl>
2024-10-21 18:28:58 +0200

    ver 0.90
    better favourites handling, now user can delete favourites and app display them in another section

--- script.js
+++ script.js
@@ -42,7 +42,50 @@ function addRecipeToFavorites(recipeId) {
         favorites.push(recipeId);
         localStorage.setItem('favorites', JSON.stringify(favorites));
         alert('Przepis został dodany do ulubionych!');
+        loadFavorites();
     } else {
         alert('Przepis jest już w ulubionych!');
     }
 }
+function removeRecipeFromFavorites(recipeId) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    favorites = favorites.filter(id => id !== recipeId);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    alert('Przepis został usunięty z ulubionych!');
+    loadFavorites();
+}
+
+function loadFavorites() {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    if (favorites.length > 0) {
+        const apiKey = config.apiKey;
+        const apiUrl = `https://api.spoonacular.com/recipes/informationBulk?ids=${favorites.join(',')}&apiKey=${apiKey}`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => displayFavorites(data))
+            .catch(error => console.error(error));
+    }
+}
+
+function displayFavorites(favorites) {
+    const container = document.getElementById('favorites-container');
+    container.innerHTML = '';
+
+    favorites.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add('recipe');
+
+        recipeCard.innerHTML = `
+            <img src="${recipe.image}" alt="${recipe.title}" />
+            <h3>${recipe.title}</h3>
+            <p>Użyte składniki: ${recipe.usedIngredientsCount}</p>
+            <p>Brakujące składniki: ${recipe.missedIngredientsCount}</p>
+            <a href="https://spoonacular.com/recipes/${recipe.title.replace(/\s+/g, '-').toLowerCase()}-${recipe.id}" target="_blank">Zobacz przepis</a>
+            <button onclick="removeRecipeFromFavorites(${recipe.id})">Usuń z ulubionych</button>
+            `;
+        container.appendChild(recipeCard);
+    });
+}
+
+window.onload = loadFavorites;
