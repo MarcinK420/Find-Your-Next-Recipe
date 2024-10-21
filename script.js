73beab7a3da5aca35453bc18ff56383eba51b069
MarcinK420 <marcin.kruk@uth.pl>
2024-10-21 09:14:57 +0200

    ver 0.80
    based functionality works now with possibility of searching recipes

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function searchRecipes() {
         .catch(error => console.error(error));
     }
 
+function displayRecipes(recipes) {
+    const container = document.getElementById('recipes-container');
+    container.innerHTML = '';
+
+    recipes.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add('recipe');
+
+        recipeCard.innerHTML = `
+            <img src="${recipe.image}" alt="${recipe.title}" />
+            <h3>${recipe.title}</h3>
+            <p>Użyte składniki: ${recipe.usedIngredientsCount}</p>
+            <p>Brakujące składniki: ${recipe.missedIngredientsCount}</p>
+            <a href="https://spoonacular.com/recipes/${recipe.title.replace(/\s+/g, '-').toLowerCase()}-${recipe.id}" target="_blank">Zobacz przepis</a>
+        `;
+        container.appendChild(recipeCard);
+    });
+}
+
